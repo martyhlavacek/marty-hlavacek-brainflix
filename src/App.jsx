b443e089f53295d92aec2410822444a1eefce8b4
martyhlavacek <141077710+martyhlavacek@users.noreply.github.com>
2023-11-22 18:52:18 -0800

    build video feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import Video from "./components/Video/Video";
 
 function App() {
   return (
     <>
       {/* setState variable for the .json data file will need to be defined here */}
       <Header />
+      <Video />
 
       {/* We'll need to broadcast the .json array values as a prop to the children component(s) use Nav's Nov.22 LAB to see the functionality */}
     </>
