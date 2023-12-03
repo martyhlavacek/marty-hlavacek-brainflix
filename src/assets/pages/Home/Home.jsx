4a6fce3267a94604d1a9fa552a5ac5c74c4f978f
martyhlavacek <141077710+martyhlavacek@users.noreply.github.com>
2023-12-03 10:40:45 -0800

    update responsiveness on upload page

--- src/assets/pages/Home/Home.jsx
+++ src/assets/pages/Home/Home.jsx
@@ -7,9 +7,9 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 
 function Home() {
-  const { videoId } = useParams();
-
   // useParams is key to being able to interact with the React Router DOM, the videoId variable helps to define the URL path that gets fed to the Router located in the app.jsx
+
+  const { videoId } = useParams();
 
   const [videos, setVideos] = useState([]);
   const [activeVideo, setActiveVideo] = useState();
@@ -25,14 +25,6 @@ function Home() {
           "https://project-2-api.herokuapp.com/videos?api_key=%225d0f98d8-23d6-4ec7-a536-72579a5dc3ff%22"
         );
         setVideos(response.data);
-        if (!videoId && response.data.length > 0) {
-          const firstVideoId = response.data[0].id;
-          const videoDetailsResponse = await axios.get(
-            `https://project-2-api.herokuapp.com/videos/${firstVideoId}?api_key=%225d0f98d8-23d6-4ec7-a536-72579a5dc3ff%22`
-          );
-          setActiveVideo(videoDetailsResponse.data);
-        }
-        // this piece of code above needs to be moved to the useEffect fetchvideo below as semantically the logic is in the wrong place
       } catch (error) {
         console.error("Error fetching videos:", error);
       }
@@ -42,21 +34,24 @@ function Home() {
   }, []);
 
   useEffect(() => {
-    const fetchVideo = async () => {
-      if (videoId) {
-        try {
-          const response = await axios.get(
-            `https://project-2-api.herokuapp.com/videos/${videoId}?api_key=%225d0f98d8-23d6-4ec7-a536-72579a5dc3ff%22`
-          );
-          setActiveVideo(response.data);
-        } catch (error) {
-          console.error("Error fetching video details:", error);
-        }
+    const fetchVideo = async (id) => {
+      try {
+        const response = await axios.get(
+          `https://project-2-api.herokuapp.com/videos/${id}?api_key=%225d0f98d8-23d6-4ec7-a536-72579a5dc3ff%22`
+        );
+        setActiveVideo(response.data);
+      } catch (error) {
+        console.error("Error fetching video details:", error);
       }
     };
 
-    fetchVideo();
-  }, [videoId]);
+    if (videoId) {
+      // this if statement checks if videoId is available from the useParems() as it is pulled from the URL as it represents the actual ID of the video
+      fetchVideo(videoId); // when the videoId is present, this function will retrieve the video the user wants to see
+    } else if (videos.length > 0) {
+      fetchVideo(videos[0].id); // this is our default/on load video that is displayed. As everything is being run through the React router this defines/fetches a video until the user selects something else
+    }
+  }, [videoId, videos]); //we need these two dependencies in the array as we need React to rerun this logic any time the videoId or videos array changes otherwise the useEffect will only run on load
 
   return (
     <div>
